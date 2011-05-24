05e7125f6e9c733cb561c737ecdb9bb571f30f2a
Siri Hedengran <siricat@gmail.com>
2011-05-23 19:34:36 -0700

    added a migration task

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,9 @@ module Depot
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
+    
+    # lib directory is now an option you have to explicitly enable by the next line:
+    config.autoload_paths += %W(#{Rails.root}/lib)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
